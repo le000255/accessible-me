f2423f00c2a4d84118233834884b235b504a938f
Gia Thinh Le (Eddie) <le000255@algonquinlive.com>
2023-10-03 14:38:14 -0400

    fix possible accessibility issues & add style & script

---
+++ js/main.js
@@ -0,0 +1,11 @@
+const mobileNav = document.getElementById('nav-links')
+
+const toggleBtnNav = document.getElementsByClassName('hamburger-btn')[0]
+console.log(toggleBtnNav);
+console.log(mobileNav);
+
+toggleBtnNav.addEventListener("click", () => {
+  console.log("Clicked");
+  mobileNav.classList.toggle('hamburger');
+
+})
